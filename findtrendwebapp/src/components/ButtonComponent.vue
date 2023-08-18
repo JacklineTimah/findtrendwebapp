c25d011f61aab7fda505503991bf494e3d2f3ad0
Jackline Timah <jacklinetimah57@gmail.com>
2023-08-18 09:27:06 +0000

    Updated

---
+++ findtrendwebapp/src/components/ButtonComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(["buttonText"])
+</script>
+
+
+<template>
+   <button>{{ buttonText }}</button> 
+</template>
+
+<style scoped>
+ button{
+    background-color: white;
+    padding: 12px 32px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 48px;
+ }
+</style>
